8ca5269a25f5aaafbebdc7cf499552658dba384f
Aurthur Musendame <aurthurmusendame@gmail.com>
2022-05-24 18:06:18 +0200

    added aside content for single article

--- frontend/src/app/weblog/article/single/single.component.ts
+++ frontend/src/app/weblog/article/single/single.component.ts
@@ -3,10 +3,19 @@ import { ActivatedRoute } from '@angular/router';
 import { catchError, map, Observable, of, startWith } from 'rxjs';
 import { IArticle } from 'src/app/dashboard/articles/article.model';
 import { ArticleService } from 'src/app/dashboard/articles/service/article.service';
+import { ICategory } from 'src/app/dashboard/categories/category.model';
+import { CategoriesService } from 'src/app/dashboard/categories/service/categories.service';
 
 interface IArticleState {
   loading: boolean;
   article?: IArticle;
+  articles?: IArticle[];
+  error?: string
+}
+
+interface ICategoryState {
+  loading: boolean;
+  data?: ICategory[];
   error?: string
 }
 
@@ -17,13 +26,15 @@ interface IArticleState {
 })
 export class SingleComponent implements OnInit {
   articleData$!: Observable<IArticleState>
+  asideNews$!: Observable<IArticleState>;
+  categories$!: Observable<ICategoryState>;
+  excludeId = "";
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private articleService: ArticleService,
-    ) { 
-
-  }
+    private categoryService: CategoriesService
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
@@ -31,6 +42,7 @@ export class SingleComponent implements OnInit {
         this.articleData$ = this.articleService.getBySlug$(params['slug'])
         .pipe(
           map(res => {
+            this.excludeId = res._id;
             return { loading: false, article: res }
           }),
           startWith({ loading: true  }),
@@ -38,6 +50,26 @@ export class SingleComponent implements OnInit {
         )
       }
     })
+
+    this.asideNews$ = this.articleService.articles$(0,6)
+    .pipe(
+      map(res => {
+        const data = res.results?.filter(x => x._id !== this.excludeId);
+        return { loading: false, articles: data || [] }
+      }),
+      startWith({ loading: true, articles: [] }),
+      catchError((error: string) => of({ loading: false, error }))
+    )
+
+    this.categories$ = this.categoryService.categories$
+    .pipe(
+      map(res => {
+        return { loading: false, data: res || [] }
+      }),
+      startWith({ loading: true, data: [] }),
+      catchError((error: string) => of({ loading: false, error }))
+    )
+
   }
 
 }
